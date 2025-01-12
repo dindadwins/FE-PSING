752945585d6f8ef25cfa352f5cb8462249f5d383
belindameylia <belinda.meylia@binus.ac.id>
2025-01-12 17:47:38 +0700

    Result 1.0

--- src/app/container/result/result.component.ts
+++ src/app/container/result/result.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './result.component.html',
   styleUrl: './result.component.scss'
 })
-export class ResultComponent {
 
+export class ResultComponent implements OnInit{
+  @Output() backToHome = new EventEmitter;
+
+  ngOnInit(): void {
+
+  }
+
+  buttonBackToHome(){
+    this.backToHome.emit()
+  }
 }
